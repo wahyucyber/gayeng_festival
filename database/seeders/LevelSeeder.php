da3c71d3316692021aa4019a4b9791d9cb8c17b8
wahyucyber <m.nur.wahyu1974@gmail.com>
2023-03-16 00:22:58 +0700

    Add model, migration and seeder level

---
+++ database/seeders/LevelSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Level;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LevelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Level::insert([
+            [
+                "name" => "Admin",
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "name" => "Customer",
+                "created_at" => now(),
+                "updated_at" => now()
+            ]
+        ]);
+    }
+}
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Level;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,7 +15,10 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $level_id = Level::where("name", "Admin")->first()["id"];
+
         User::create([
+            "level_id" => $level_id,
             "name" => "Cyber Tech",
             "email" => "admin@gmail.com",
             "password" => Hash::make("12345")
