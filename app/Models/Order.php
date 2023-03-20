3bac110bba9258380a6f3d70947af26443450227
wahyucyber <m.nur.wahyu1974@gmail.com>
2023-03-20 14:04:05 +0700

    Add model and migration order

---
+++ app/Models/Order.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ["user_id", "invoice", "pay", "payment_type", "payment_response", "payment_status"];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2023_03_20_063633_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id");
+            $table->string("invoice")->unique();
+            $table->integer("pay");
+            $table->string("payment_type")->comment("bri_va, bni_va, mandiri_va, bca_va, permata_va, qris, indomaret and alfamart");
+            $table->string("payment_response")->nullable();
+            $table->string("payment_status")->comment("settlement, pending, expire, cancel, deny and refund")->default("pending");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
