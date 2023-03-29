b783adc0393c50aaf0c3435b46c97c9f8c35e017
wahyucyber <m.nur.wahyu1974@gmail.com>
2023-03-29 13:51:30 +0700

    Add column status from tickets table

--- database/migrations/2023_03_20_072752_create_tickets_table.php
+++ database/migrations/2023_03_20_072752_create_tickets_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId("order_item_id");
             $table->string("code")->unique();
+            $table->string("status")->comment("pending or settlement")->default("pending");
             $table->timestamps();
         });
     }
