55d655e964132e5eaa4ca8c7eb4ed352127e3ac3
wahyucyber <m.nur.wahyu1974@gmail.com>
2023-04-12 00:47:24 +0700

    Update dashboard admin

--- resources/views/_layout/app.blade.php
+++ resources/views/_layout/app.blade.php
@@ -78,7 +78,7 @@
 
                                         <div class="page-title-right">
                                             <ol class="breadcrumb m-0">
-                                                <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
+                                                <li class="breadcrumb-item"><a href="{{ route("admin.dashboard") }}">Dashboard</a></li>
                                                 <li class="breadcrumb-item active">{{ $title }}</li>
                                             </ol>
                                         </div>
