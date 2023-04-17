46e60092c58c140ebba108b9d427fdad473e4aad
wahyucyber <m.nur.wahyu1974@gmail.com>
2023-04-17 13:16:01 +0700

    Create column term_and_condition from events table

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -9,7 +9,7 @@ class Event extends Model
 {
     use HasFactory;
 
-    protected $fillable = ["user_id", "category_id", "picture", "slug", "title", "start_time", "end_time", "price", "stock", "description", "location"];
+    protected $fillable = ["user_id", "category_id", "picture", "slug", "title", "start_time", "end_time", "price", "stock", "description", "location", "term_and_condition"];
 
     public function user()
     {
