f75dc4847e79be0f09b2d4e600b5f864d53c6f6e
wahyucyber <m.nur.wahyu1974@gmail.com>
2023-04-17 12:53:08 +0700

    Create column category_id from events table

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -9,7 +9,7 @@ class Event extends Model
 {
     use HasFactory;
 
-    protected $fillable = ["user_id", "picture", "slug", "title", "date", "start_time", "end_time", "price", "stock", "description"];
+    protected $fillable = ["user_id", "category_id", "picture", "slug", "title", "date", "start_time", "end_time", "price", "stock", "description"];
 
     public function user()
     {
@@ -24,5 +24,10 @@ class Event extends Model
     public function order_items()
     {
         return $this->hasMany(Order_item::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 }
