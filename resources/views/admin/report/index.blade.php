0919cd473199f20726b4799d87d07efb9f05435e
wahyucyber <m.nur.wahyu1974@gmail.com>
2023-04-19 20:57:25 +0700

    Update report on dashboard admin

--- resources/views/admin/report/index.blade.php
+++ resources/views/admin/report/index.blade.php
@@ -149,7 +149,7 @@
                             }
                         },
                         {
-                            data: `user.name`,
+                            data: `name`,
                             sort: false
                         }
                     ]
