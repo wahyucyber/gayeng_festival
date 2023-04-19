f074d0b0986089241d9c385192496bb97929dece
wahyucyber <m.nur.wahyu1974@gmail.com>
2023-04-19 16:13:09 +0700

    Add column whatsapp from tickets table

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -9,7 +9,7 @@ class Ticket extends Model
 {
     use HasFactory;
 
-    protected $fillable = ["order_id", "identity_id", "code", "status", "name", "identity", "email"];
+    protected $fillable = ["order_id", "identity_id", "code", "status", "name", "identity", "email", "whatsapp"];
 
     public function order()
     {
