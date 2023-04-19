758aa5ba9f3b4d18bc0bde795128576e8dae81e2
wahyucyber <m.nur.wahyu1974@gmail.com>
2023-04-19 15:49:19 +0700

    Add column email and whatsapp from orders table

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,7 +9,7 @@ class Order extends Model
 {
     use HasFactory;
 
-    protected $fillable = ["event_ticket_id", "identity_id", "invoice_index", "invoice", "pay", "admin_fee", "total_pay", "payment_type", "payment_response", "payment_status", "name", "indentity"];
+    protected $fillable = ["event_ticket_id", "identity_id", "invoice_index", "invoice", "pay", "admin_fee", "total_pay", "payment_type", "payment_response", "payment_status", "name", "indentity", "email", "whatsapp"];
 
     public function order_items()
     {
